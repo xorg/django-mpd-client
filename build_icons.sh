77533fc6c41cbce481bb938cd974e6f9b4909e6a
Stefan Schneider <stefan.schneider@intergga.ch>
2015-03-10 13:16:04 +0100

    Moved imagemagick commands from travis config to separate script

---
+++ build_icons.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+convert resources/Next.xcf mpd/mobile/media/img/next.png
+convert resources/Previous.xcf mpd/mobile/media/img/next.png
+convert resources/Play.xcf mpd/mobile/media/img/play.png
+convert resources/Pause.xcf mpd/mobile/media/img/pause.png
+convert resources/Delete.xcf mpd/mobile/media/img/delete.png
